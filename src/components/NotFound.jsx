db5904cc655f73211ca2bc1c76f99931ad8add0c
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-11 15:38:41 +0200

    changed the not-found-section direction according to the lang

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -11,17 +11,19 @@ import { ImWarning } from "react-icons/im";
 const NotFound = () => {
   const { t } = useTranslation();
 
-  const { shipmentNum } = useSelector((store) => store.track);
+  const { shipmentNum, isArabic } = useSelector((store) => store.track);
 
   return (
     <>
-      <div className="error-container">
+      <div
+        className={isArabic ? "error-container error-rtl" : "error-container"}
+      >
         <p className="shipment-num">
           {t("ui_shipment_no")} {shipmentNum.payload}
         </p>
         <div className="error-info">
           <ImWarning />
-          <p>{t("ui_not_found_msg")}</p>
+          <p className={isArabic ? "p-rtl" : ""}>{t("ui_not_found_msg")}</p>
         </div>
       </div>
     </>
